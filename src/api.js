2ba2adb585eb3b2de665daaa750cb95a5770e0e4
0xThamer <thamertx0@gmail.com>
2018-06-23 22:00:55 +0300

    add getPosts

--- src/api.js
+++ src/api.js
@@ -3,14 +3,17 @@ import axios from 'axios'
 axios.defaults.baseURL = 'https://coretabs-happy.herokuapp.com'
 
 const appService = {
-  getPosts() {
-    return new Promise((resolve) => {
-      axios.get(`/api/v1/posts/`)
-      .then(response => {
-        resolve(response.data)
-      })
+  getPosts () {
+    return new Promise((resolve, reject) => {
+      axios.get('/api/v1/posts/')
+        .then(res => {
+          resolve(res.data)
+        })
+        .catch(error => {
+          reject(error)
+        })
     })
   }
 }
 
- export default appService
+export default appService
