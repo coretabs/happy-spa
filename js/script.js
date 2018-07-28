8abab9ecaa191623881ef63cff78e9ae9434a667
Unknown <a-ahmed987@outlook.com>
2018-07-28 22:13:55 +0300

    fixed manage links page footer

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ var newItem = document.getElementById("RABT");
 newItem.addEventListener('click' , addItem);
 
 function addItem () {
-    myMenu.innerHTML += "<li><div class='field linkMSM'><div class='linkSSM'><div class='control linkSM-para'><div class='select'><select><option>موقع</option><option>facebook</option><option>twitter</option><option>instagram</option><option>telegram</option><option>githun</option><option>اخرى</option></select></div></div><p class='control has-icons-left linkSM'><input class='input linkSM-input' type='link' placeholder='https://www.wep-site.com'><span class='icon is-small RBR'><i class='fas fa-eye'></i></span></p></div></div></li>";
+    myMenu.innerHTML += "<li><div class='field linkMSM'><div class='linkSSM'><div class='control linkSM-para'><div class='select'><select><option>موقع</option><option>facebook</option><option>twitter</option><option>instagram</option><option>telegram</option><option>githun</option><option>اخرى</option></select></div></div><p class='control has-icons-left linkSM'><input class='input linkSM-input' type='link' placeholder='https://www.wep-site.com'><span class='icon is-small RBR'><i class='fas fa-trash'></i></span></p></div></div></li>";
 }
-
 
 var pwd = document.getElementById('pwd');
 var eye = document.getElementById('eye');
