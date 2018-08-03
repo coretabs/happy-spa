209631ae1c6818a823a3eb5674f281af8196015d
Unknown <a-ahmed987@outlook.com>
2018-08-03 21:55:41 +0300

    change links from email to links

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var newItem = document.getElementById("RABT");
 newItem.addEventListener('click' , addItem);
 
 function addItem () {
-    myMenu.innerHTML += "<div class='enterMail'><p class='paraContent'>البريد الإلكتروني الإحتياطي</p><div class='field'><p class='control has-icons-right'><input class='input' type='email' placeholder='أدخل بريدك الإلكتروني'><span class='icon is-small is-right'><i class='fas fa-envelope'></i></span></p></div></div>";
+    myMenu.innerHTML += "<li><div class='field linkMSM'><div class='linkSSM'><div class='control linkSM-para'><div class='select'><select><option>موقع</option><option>facebook</option><option>twitter</option><option>instagram</option><option>telegram</option><option>githun</option><option>اخرى</option></select></div></div><p class='control has-icons-left linkSM'><input class='input linkSM-input' type='link' placeholder='https://www.wep-site.com'><span class='icon is-small RBR'><i class='fas fa-trash'></i></span></p></div></div></li>";
 }
 
 var pwd = document.getElementById('pwd');
