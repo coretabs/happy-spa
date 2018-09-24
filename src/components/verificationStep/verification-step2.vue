131b2db77369f9ad514169123306658d75223755
roabaot <a-ahmed987@outlook.com>
2018-09-24 22:45:14 +0300

    add verification-step2.vue

---
+++ src/components/verificationStep/verification-step2.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="grayContentPage">
+
+        <div class="mianHeader">
+            <a href="#">
+                <i class="material-icons">arrow_forward</i>
+            </a>
+            <p>تفعيل حسابك</p>
+            <img src="image/mainWhite.png">
+        </div>
+        
+        <div class="contentPageHeader contentCenter">
+
+            <img src="image/undraw-mail.svg" class="mainPoster">
+            
+            <p class="paraContent">تم إرسال رسالة التفعيل إلى بريدك الإلكتروني, يرجى تفقد الرسالة لتفعيل حسابك</p>
+            
+            <div class="field">
+            <p class="control has-icons-left">
+                <button class="button is-success">
+                تفعيل
+                </button>
+                <span class="icon iconCenter is-small is-left">
+                <i class="material-icons">arrow_back</i>
+                </span>
+            </p>
+            </div>
+            
+            <div class="paraFooter">
+                <p>
+                    لم تصلك رسالة التفعيل؟
+                    <a href="#">أعد إرسال الرسالة</a>
+                </p>
+            </div>
+            
+        </div>
+
+    </div>
+</template>
