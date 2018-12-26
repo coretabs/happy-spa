3d4ea226784d40880c0f159b00bb9b2996917557
Fethi TChi <adm4algnw@gmail.com>
2018-12-26 21:22:31 +0100

    move to grinn.herokuapp.com api

--- src/api/api.js
+++ src/api/api.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 import Vue from 'vue'
 import Cookies from 'js-cookie'
 
-axios.defaults.baseURL = 'https://corefun.herokuapp.com'
+axios.defaults.baseURL = 'https://grinn.herokuapp.com'
 Cookies.getJSON('logedinUser') ? axios.defaults.headers.common['authorization'] = `Bearer ${Cookies.getJSON('logedinUser').token}` : ''
 window.axios = axios
 
